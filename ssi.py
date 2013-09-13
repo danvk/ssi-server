37d994bd04f88227e11cb269852a8aa56bed4923
Dave Collins <dave@hopest.net>
2013-09-13 10:04:12 +1000

    Added support for shtml files Allow include "file"

--- ssi.py
+++ ssi.py
@@ -3,12 +3,19 @@ Shared code for ssi_server.py and ssi_expander.py.
 '''
 
 import re
+import os.path
 
-def InlineIncludes(path):
+def InlineIncludes(path, web_path):
   """Read a file, expanding <!-- #include --> statements."""
+  def get_include_file_content(x):
+    file_to_read =  x.group(2)
+    if  len(os.path.dirname(web_path)) >2:
+       file_to_read = os.path.join(os.path.dirname(web_path),file_to_read)[1:]
+    return file(file_to_read).read()
+    
   content = file(path).read()
-  content = re.sub(r'<!-- *#include *virtual=[\'"]([^\'"]+)[\'"] *-->',
-      lambda x: file(x.group(1)).read(),
+  content = re.sub(r'<!-- *#include *(virtual|file)=[\'"]([^\'"]+)[\'"] *-->',
+      get_include_file_content,
       content)
   return content
 
