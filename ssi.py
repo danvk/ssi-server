945b28f867fbb421cb34e030e045aaea717d9592
Rainer Koirikivi <rainer@koirikivi.fi>
2016-01-30 01:11:24 +0200

    Add support for recursive SSI

--- ssi.py
+++ ssi.py
@@ -9,11 +9,14 @@ def InlineIncludes(path, web_path):
   """Read a file, expanding <!-- #include --> statements."""
   def get_include_file_content(x):
     file_to_read =  x.group(2)
+    recursive_web_path = web_path
     if  len(os.path.dirname(web_path)) >2:
        file_to_read = os.path.join(os.path.dirname(web_path),file_to_read)[1:]
+       recursive_web_path = "/%s/" % os.path.dirname(file_to_read)
     if os.path.exists(file_to_read):
-        return open(file_to_read).read()
-    
+      # Recursively process ssi calls in the included file
+      return InlineIncludes(file_to_read, recursive_web_path)
+
   content = open(path).read()
   content = re.sub(r'<!-- *#include *(virtual|file)=[\'"]([^\'"]+)[\'"] *-->',
       get_include_file_content,
